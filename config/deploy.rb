1f65fc76a06cb98e4118889186e60254577edfc4
activeliang <activeliang@gmail.com>
2019-11-11 16:21:14 +0800

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,6 +37,8 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 set :passenger_restart_with_touch, true
 set :keep_releases, 5
 
+set :rvm_ruby_version, '2.4.4'
+
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
